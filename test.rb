44c9ba6f1aa8e30a6d01d8dd7ee72f844ad4d2eb
julia frederico <magalhaesjulia@id.uff.br>
2021-06-30 16:54:56 -0300

    question 1 disible numbers method - insert conditional to check all variables are integers

--- test.rb
+++ test.rb
@@ -3,12 +3,16 @@
 # números divisíveis por w entre x e y.
 
 def return_divisible_numbers(x, y, w)
-  # [EN] Creating an array of integers from X to Y
-  # [PT] Criando uma array de integers entre X e Y
-  numbers = (x..y).to_a
-  # [EN] Iterating over the array to select the numbers where the remainder of the division by W is equal to 0
-  # [PT] Iterando sobre a array de números para selecionar aqueles cujo o resto da divisão por W é igual a 0
-  numbers.select {|number| number % w == 0 }
+  if x.is_a?(Integer) && y.is_a?(Integer) && w.is_a?(Integer)
+    # [EN] Creating an array of integers from X to Y
+    # [PT] Criando uma array de integers entre X e Y
+    numbers = (x..y).to_a
+    # [EN] Iterating over the array to select the numbers where the remainder of the division by W is equal to 0
+    # [PT] Iterando sobre a array de números para selecionar aqueles cujo o resto da divisão por W é igual a 0
+    numbers.select {|number| number % w == 0 }
+  else
+    puts "Todos os argumentos passados devem ser números inteiros."
+  end
 end
 
 ## Q.2
